f3a95112e76b251adb185bd2871a9c86353d7201
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-31 19:42:39 +0100

    Cambios realizados Ejercicio_6.py y del test_6.py

--- src/Ejercicio_6.py
+++ src/Ejercicio_6.py
@@ -4,30 +4,39 @@
 # asignaturas aprobadas. Al final el programa debe mostrar por pantalla las 
 # asignaturas que el usuario tiene que repetir.
 
-def asignaturasRepetidas(asignaturas, notas_input):
-    asignaturas_repetir = []
-    indice = 0
-    while indice < len(asignaturas):
-        asignatura = asignaturas[indice]
-        try:
-            nota = float(notas_input[indice])
-            if 0 <= nota <= 10:
-                if nota < 5.0:
-                    asignaturas_repetir.append(asignatura)
+def obtener_notas(asignaturas):
+    notas = []
+    for asignatura in asignaturas:
+        nota_valida = False
+        while not nota_valida:
+            nota_input = input(f"Ingrese la nota de {asignatura}: ")
+            if nota_input.replace(".", "", 1).isdigit():
+                nota = float(nota_input)
+                if 0 <= nota <= 10:
+                    notas.append(nota)
+                    nota_valida = True
+                else:
+                    print("Error: La nota debe estar entre 0 y 10.")
             else:
-                print(f"Error: La nota de {asignatura} debe estar entre 0 y 10.")
-        except ValueError:
-            print(f"Error: Por favor, ingresa un número válido para la nota de {asignatura}.")
-        indice += 1
+                print("Error: Por favor, ingrese un número válido para la nota.")
+                return []
+    return notas
+
+def asignaturas_repetir(asignaturas, notas):
+    asignaturas_repetir = []
+    for i in range(len(asignaturas)):
+        nota_str = str(notas[i])
+        if nota_str.replace(".", "", 1).isdigit():
+            nota_float = float(nota_str)
+            if 0 <= nota_float < 5.0:
+                asignaturas_repetir.append(asignaturas[i])
     return asignaturas_repetir
 
 if __name__ == "__main__":
-    # Entrada
-    asignaturas_input = input("Ingresa las asignaturas del curso separadas por comas: ").split(',')
-    notas_input = input("Ingresa las notas correspondientes separadas por comas: ").split(',')
-    # Proceso
-    asignaturas_repetir = asignaturasRepetidas(asignaturas_input, notas_input)
-    # Salida
-    print("Asignaturas que debes repetir:")
+    asignaturas = input("Ingrese las asignaturas del curso separadas por comas: ").split(',')
+    notas = obtener_notas(asignaturas)
+    asignaturas_repetir = asignaturas_repetir(asignaturas, notas)
+    
+    print("Asignaturas que debe repetir:")
     for asignatura in asignaturas_repetir:
         print(asignatura)
--- test/test_ej6.py
+++ test/test_ej6.py
@@ -1,44 +1,50 @@
-from src.Ejercicio_6 import asignaturasRepetidas
+from src.Ejercicio_6 import asignaturas_repetir
 
-def test_asignaturas_repetidas():
-    # Caso de prueba con notas válidas
+def test_asignaturas_repetir():
+    # Caso de prueba con notas válidas y asignaturas a repetir
     asignaturas = ["Matemáticas", "Física", "Química"]
-    notas_input = ["4.5", "6.0", "3.8"]
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == ["Matemáticas", "Química"]
+    notas_input = [4.5, 6.0, 3.8]
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == ["Matemáticas", "Química"]
 
-    # Caso de prueba con notas inválidas
+    # Caso de prueba con notas válidas y sin asignaturas a repetir
     asignaturas = ["Matemáticas", "Física", "Química"]
-    notas_input = ["abc", "6.0", "3.8"]
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == []
-
-    # Caso de prueba con notas fuera de rango
-    asignaturas = ["Matemáticas", "Física", "Química"]
-    notas_input = ["11.0", "6.0", "-3.8"]
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == []
-
-    # Caso de prueba con notas válidas pero sin asignaturas
-    asignaturas = []
-    notas_input = []
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == []
-
-    # Caso de prueba con notas válidas y asignaturas pero sin asignaturas a repetir
-    asignaturas = ["Matemáticas", "Física", "Química"]
-    notas_input = ["8.5", "9.0", "7.8"]
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == []
-
-    # Caso de prueba con notas válidas y asignaturas aprobadas
-    asignaturas = ["Matemáticas", "Física", "Química"]
-    notas_input = ["8.5", "4.5", "3.8"]
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == ["Física", "Química"]
+    notas_input = [8.5, 9.0, 7.8]
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == []
 
     # Caso de prueba con notas válidas, asignaturas aprobadas y asignaturas vacías
     asignaturas = []
     notas_input = []
-    asignaturas_repetir = asignaturasRepetidas(asignaturas, notas_input)
-    assert asignaturas_repetir == []
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == []
+
+    # Caso de prueba con notas válidas y asignaturas aprobadas
+    asignaturas = ["Matemáticas", "Física", "Química"]
+    notas_input = [8.5, 4.5, 3.8]
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == ["Física", "Química"]
+
+    # Caso de prueba con notas inválidas
+    asignaturas = ["Matemáticas", "Física", "Química"]
+    notas_input = ["abc", 6.0, -3.8]
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == []
+
+    # Caso de prueba con notas fuera de rango
+    asignaturas = ["Matemáticas", "Física", "Química"]
+    notas_input = [11.0, 6.0, -3.8]
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == []
+
+    # Caso de prueba con notas válidas pero sin asignaturas
+    asignaturas = []
+    notas_input = []
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == []
+
+    # Caso de prueba con notas inválidas y asignaturas
+    asignaturas = ["Matemáticas", "Física", "Química"]
+    notas_input = ["abc", "def", "ghi"]
+    asignaturas_repetidas_resultado = asignaturas_repetir(asignaturas, notas_input)
+    assert asignaturas_repetidas_resultado == []
