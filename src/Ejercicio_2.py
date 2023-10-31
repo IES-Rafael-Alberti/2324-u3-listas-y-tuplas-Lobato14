b735ac1ba6d24d1ad1f516858b9121997da87098
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-31 14:07:11 +0100

    Cambios realizados en Ejercicio_2.py y en test_ej2.py

--- src/Ejercicio_2.py
+++ src/Ejercicio_2.py
@@ -6,25 +6,39 @@
 def generar_mensajes_asignaturas(asignaturas):
     mensajes = []
     for asignatura in asignaturas:
-        mensajes.append("Yo estudio " + asignatura)
+        if asignatura.isalpha() and asignatura != "":
+            mensajes.append("Yo estudio " + asignatura)
+        else:
+            raise ValueError("Error: Ingresa un nombre válido para la asignatura.")
     return "\n".join(mensajes)
 
 def mostrar_asignaturas(asignaturas):
-    return generar_mensajes_asignaturas(asignaturas)
+    try:
+        mensajes = generar_mensajes_asignaturas(asignaturas)
+        return mensajes
+    except Exception as e:
+        return "Error: " + str(e)
 
 if __name__ == "__main__":
+    # Entrada
     asignaturas = []
     introducir_asig = "si"
+    # Procesamiento
     while introducir_asig.lower() == "si":
         asignatura = input("Escribe una asignatura: ")
-        if asignatura.isalpha() and asignatura != "":
-            asignaturas.append(asignatura)
-            introducir_asig = input("¿Deseas introducir otra asignatura a la lista? (si/no): ")
-            while introducir_asig.lower() not in ["si", "no"]:
-                print("Error: Por favor, ingresa 'si' o 'no'.")
+        try:
+            if asignatura.isalpha() and asignatura != "":
+                asignaturas.append(asignatura)
                 introducir_asig = input("¿Deseas introducir otra asignatura a la lista? (si/no): ")
-        else:
-            print("Error: Ingresa un nombre válido para la asignatura.")
+                while introducir_asig.lower() not in ["si", "no"]:
+                    print("Error: Por favor, ingresa 'si' o 'no'.")
+                    introducir_asig = input("¿Deseas introducir otra asignatura a la lista? (si/no): ")
+            else:
+                raise ValueError("Ingresa un nombre válido para la asignatura.")
+        except ValueError as ve:
+            print("Error:", ve)
             introducir_asig = "si"
+
     mensajes_asignaturas = mostrar_asignaturas(asignaturas)
+    # Salida
     print(mensajes_asignaturas)
