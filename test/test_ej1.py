d0e7d10e33f337546312f3b4aacefa2309b06582
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-31 11:24:58 +0100

    Creacion y realizacion del Ejercicio_1.py y del test_ej1.py

---
+++ test/test_ej1.py
@@ -0,0 +1,5 @@
+from src.Ejercicio_1 import mostrarAsignaturas
+
+def test_mostrarAsignaturas():
+    assert mostrarAsignaturas
+    (["Matemáticas", "Física", "Química", "Historia", "Lengua"]) == ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
